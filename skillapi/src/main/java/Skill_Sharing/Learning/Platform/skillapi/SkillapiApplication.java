296a72cac4b0c8372b509bb0549466416847e39c
ChamodyaIshani99 <138350748+ChamodyaIshani99@users.noreply.github.com>
2025-04-10 08:06:42 +0530

    create repo

---
+++ skillapi/src/main/java/Skill_Sharing/Learning/Platform/skillapi/SkillapiApplication.java
@@ -0,0 +1,13 @@
+package Skill_Sharing.Learning.Platform.skillapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillapiApplication.class, args);
+	}
+
+}
---
+++ skillapi/src/test/java/Skill_Sharing/Learning/Platform/skillapi/SkillapiApplicationTests.java
@@ -0,0 +1,13 @@
+package Skill_Sharing.Learning.Platform.skillapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
