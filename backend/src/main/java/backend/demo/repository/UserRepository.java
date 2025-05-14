6d82b8c4305f2a8c701ced0e89da285f3080ce83
lahiru <www.lahiruniroosh@gmail.com>
2025-05-14 10:49:33 +0530

    updated repoitory file added -lahiru

---
+++ backend/src/main/java/backend/demo/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.photohub.repository;
+
+import com.photohub.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByEmail(String email);
+}
